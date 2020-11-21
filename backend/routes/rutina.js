ce321108242abf7dcd7e1c7a80e1dc617ee3c14f
Juan David Londono <66636645+juandavid200@users.noreply.github.com>
2020-11-21 12:24:17 -0600

    Update rutina.js

--- backend/routes/rutina.js
+++ backend/routes/rutina.js
@@ -3,71 +3,77 @@ const router = express.Router();
 const mysqlConnection =require('../db/db');
 
 
-router.get('/',(req,res)=>{
-  res.send('Si funciona')
-})
-
-        // tipo_usuario
-    //Petición get
-router.get('/rutina',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM rutina',
-    (err,rows,fields)=>{
-      if(!err)
-     {
-       res.json(rows);
-     }else{
-       console.log(err);
-     }
-    })
-})
-//Petición post
-   router.post('/rutina', (req, res) => {
-    const {ID,alarma,fecha,hora} = req.body
-    let rutina = [ID,alarma,fecha,hora];
-    let programarNuevaRutina = `INSERT INTO rutina VALUES (?,?,?,?);`
-
-   mysqlConnection.query(programarNuevaRutina,rutina, (err,results,fields) => {
-     if(err){
-       return console.error(err.message);
-     }
-     res.json({message:`Rutina creada`})
-   });
-  });
-
-//Petición put
-  router.put('/rutina/:ID', (req,res) => {
-  const {alarma,fecha,hora} = req.body
-  const { ID } = req.params 
-
-mysqlConnection.query(`UPDATE rutina
-                       SET alarma=?,fecha=?,hora=?
-                       WHERE ID = ?`,
-                       [alarma,fecha,hora,ID], (err, rows,fields) => {
-   if(!err){
-    res.json({status: `Rutina actualizada `});
-   }else{
-     console.log(err);
-   }
-});
-});
-
-  //PETICIÓN DELETE - ELIMINACIÓN DE DATOS
-  router.delete('/rutina/:ID', (req,res) => {
-    const { ID } = req.params;
-    mysqlConnection.query(`DELETE FROM rutina WHERE ID =?`,[ID],(err,rows,fields) => {
-      if("!err"){
-        res.json({status: `Rutina eliminada `})
-      }else{
-        console.log(err);
+router.get('/', (req, res) => {
+  mysqlConnection.query('SELECT * FROM Rutina', (err,rows,fields)=>{
+  
+      if(!err){
+          res.json(rows);
+      } else {
+          console.log(err);
       }
-    });
-  });
-
-/// contenido
-
-module.exports = router;
-
-
-/// contenido
-
-module.exports = router;
+  
+  })//fin conexion consultada
+  });//fin mostrar todas las Rutina
+  
+  router.get('/Rutina/id:', (req, res) => {
+  const {id}= req.params;
+  mysqlConnection.query('SELECT * FROM Rutina WHERE id=?', [id], (err,rows,fields)=>{
+      if(!err){
+          res.json(rows[0]);
+      } else {
+          console.log(err);
+      }
+  
+  })//fin conexion consultada por id 
+  
+  });//fin buscar rutina por su id  
+      
+  //Agregar una nueva rutina
+  
+  router.post("/nueva-Rutina", (req, res) => {
+      const { alarma, fecha, hora } = req.body;
+      let Rutinarray= [ alarma, fecha, hora ];
+      let nuevaRutina='INSERT INTO Rutina {alarma, fecha, hora}VALUES(?,?,?)';
+      mysqlConnection.query(nuevaRutina, Rutinarray,(err,results,fields)=>{
+      
+      if(err){
+          return console.error(err);
+      }else{
+          res.json({message:nombre + 'Rutina creada'})
+      }
+  
+  })
+  }); //fin guardar nueva Rutina
+  
+  router.put("/Rutina/:id", (req, res) => {
+      const { alarma, fecha, hora } = req.body;
+      const {id} = req.params;
+      mysqlConnection.query('UPDATE Rutina set alarma=?,fecha=?,hora=? WHERE id=?',
+      [alarma, fecha, hora, id],(err, rows, fields)=>{
+          if(err){
+              res.json({status:'alarma' +alarma + 'actualizado'})
+          }else{
+              console.log(err);
+          }//fin si
+      })//fin consulta 
+  
+     
+      });//fin modificar una Rutina
+  
+      router.delete("/Rutina/:id", (req, res) => {
+          const {id} = req.params;
+          mysqlConnection.query('DELETE FROM Rutina WHERE id=?',
+          [id],(err, rows, fields)=>{
+              if(err){
+                  res.json({status:'Rutina eliminada'})
+              }else{
+                  console.log(err);
+              }//fin si
+          });//fin elimnar
+      
+         
+          });//fin eliminar una Rutina
+  
+  module.exports = router
+  
+  
