fd223eb16b3e197514c9e04713eaaae88ade11e3
Samuelcoloradovelasquez <66636073+Samuelcoloradovelasquez@users.noreply.github.com>
2020-11-21 13:47:24 -0500

    Update ejercicio_usuario.js

--- backend/routes/ejercicio_usuario.js
+++ backend/routes/ejercicio_usuario.js
@@ -1,3 +1,5 @@
 const express = require('express');//tabNIne
 const router = express.Router();
 const mysqlConnection =require('../db/db');
+router.get('/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM ejercicio_usuario', (err,rows,fields)=>{
