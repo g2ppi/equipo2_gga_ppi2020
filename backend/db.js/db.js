6c0993548e5018dcb1036c1027b4b4f0132dc20f
Samuelcoloradovelasquez <66636073+Samuelcoloradovelasquez@users.noreply.github.com>
2020-11-21 13:28:49 -0500

    Update db.js

--- backend/db.js/db.js
+++ backend/db.js/db.js
@@ -3,24 +3,3 @@ const mysqlConnection= mysql.createConnection({
     // db creada https://console.clever-cloud.com/
 host:'bpegmilzjbod5uj5dold-mysql.services.clever-cloud.com',
 user:'umsrzew6nbbwl8ff',
-password:'yD0lQug8dkxePI8dmoIG',
-database:'bpegmilzjbod5uj5dold',
-multipleStatements:true
-/*
-host:'181.133.136.83',
-user:'mediatecnica',
-password:'123',
-database:'lab_mediatecnica',
-*/
-});
-
-mysqlConnection.connect(function(err){
-if(err){
-    console.error(err);
-    return;
-
-}else{
-console.log ('base de datos conectada ');
-}
-})
-module.exports=mysqlConnection;
