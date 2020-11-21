93b2b819bffb49cc3a35d767a668dfb8d80b9c25
Juan David Londono <66636645+juandavid200@users.noreply.github.com>
2020-11-21 12:36:07 -0600

    tipo 6

--- backend/routes/tipo_usuario.js
+++ backend/routes/tipo_usuario.js
@@ -22,3 +22,8 @@ const {id}= req.params;
 
   mysqlConnection.query('SELECT * FROM tipousuario WHERE id= ?', [id], (err,rows,fields)=>{
     if(!err){
+
+        res.json(rows[0]);
+    } else {
+        console.log(err);
+    }
