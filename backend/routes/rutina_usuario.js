e54ba3649c51dde1be42f6daf14a3fc5c092bd80
Kevin Santiago Monsalve Agudelo <66635759+KevinSanMonsalve@users.noreply.github.com>
2020-11-21 13:42:50 -0500

    Update rutina_usuario.js

--- backend/routes/rutina_usuario.js
+++ backend/routes/rutina_usuario.js
@@ -43,4 +43,6 @@ router.post("/nueva-Rutina_usuario", (req, res) => {
             res.json({message:nombre + 'Rutina creada'})
         }
           
+          })
+    }); //fin guardar nueva Rutina
 module.exports = router;
