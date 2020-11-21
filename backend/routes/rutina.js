0ee2696fb6aea9df7a89875f74ed376e6eb8acba
Juan David Londono <66636645+juandavid200@users.noreply.github.com>
2020-11-21 12:47:41 -0600

    edit 2

--- backend/routes/rutina.js
+++ backend/routes/rutina.js
@@ -1,3 +1,13 @@
 const express = require('express');//tabNIne
 const router = express.Router();
 const mysqlConnection =require('../db/db');
+
+router.get('/', (req, res) => {
+  mysqlConnection.query('SELECT * FROM Rutina', (err,rows,fields)=>{
+  
+      if(!err){
+          res.json(rows);
+      } else {
+          console.log(err);
+      }
+  
