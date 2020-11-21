28617f38b57e89faa724ca1252f9cd59722529b1
Juan David Londono <66636645+juandavid200@users.noreply.github.com>
2020-11-21 12:31:17 -0600

    tipo usuario 1

--- backend/routes/tipo_usuario.js
+++ backend/routes/tipo_usuario.js
@@ -4,3 +4,10 @@ const mysqlConnection = require ('../db/db');
 
 router.get('/', (req, res) => {
 mysqlConnection.query('SELECT * FROM tipousuario', (err,rows,fields)=>{
+
+  
+    if(!err){
+        res.json(rows);
+    } else {
+        console.log(err);
+    }
