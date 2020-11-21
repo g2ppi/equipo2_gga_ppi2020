67940822b2892d79358be7aa42e60b225e9fbb9f
Samuelcoloradovelasquez <66636073+Samuelcoloradovelasquez@users.noreply.github.com>
2020-11-21 13:49:22 -0500

    Update ejercicio_usuario.js

--- backend/routes/ejercicio_usuario.js
+++ backend/routes/ejercicio_usuario.js
@@ -14,3 +14,7 @@ console.log(err);
     const {id}= req.params;
      mysqlConnection.query('SELECT * FROM ejercicio_usuario WHERE id= ?', [id], (err,rows,fields)=>{
         if(!err){
+ res.json(rows[0]);
+        } else {
+            console.log(err);
+        }
