8ded41ef3b7a148814829f11d274da625a2d7a2a
Samuel Stiven Londoño Grisales <66587932+g2ppi@users.noreply.github.com>
2020-11-21 14:21:56 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,50 +3,30 @@ const app = express();
 
 // definir todos los componentes, modulos app
 const ejercicio= require('./routes/ejercicio');// crud
-
 const ejercicio_usuario = require('./routes/ejercicio_usuario');// crud
-
 const rutina = require('./routes/rutina');// crud
-
 const rutina_usuario = require('./routes/rutina_usuario');// crud
-
 const usuario = require('./routes/usuario');// crud
-
 const tipousuario = require('./routes/tipousuario');// crud
-
 //modificar const
 
 
 //ajustes
-
-
 app.set('port',3000)
 
-
-
 // Middleware
-
 app.use(express.json());
 
 // ajustess
-
 app.use('/api/',ejercicio);
-
 app.use('/api/',ejercicio_usuario);
-
 app.use('/api/',rutina);
-
 app.use('/api/',rutina_usuario);
-
 app.use('/api/',usuario);
-
 app.use('/api/',tipousuario);
 
 //modificar api
-
 app.listen(app.get('port'),()=>{
-
 console.log(`Servidor corriendo en puerto  ${app.get
-
 ('port')}`)
 })
