fbfb01dd7c98407e56b61a7f7023ca494f7f1909
Samuel Stiven Londoño Grisales <66587932+g2ppi@users.noreply.github.com>
2020-11-21 14:24:16 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 
 // definir todos los componentes, modulos app
+
 const ejercicio= require('./routes/ejercicio');// crud
 const ejercicio_usuario = require('./routes/ejercicio_usuario');// crud
 const rutina = require('./routes/rutina');// crud
