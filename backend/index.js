e89642d6612c057faa8ff43746b67377edb5e1a6
Samuel Stiven Londoño Grisales <66587932+g2ppi@users.noreply.github.com>
2020-11-21 14:26:15 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use('/api/',ejercicio);
 
 app.use('/api/',ejercicio_usuario);
+
 app.use('/api/',rutina);
 app.use('/api/',rutina_usuario);
 app.use('/api/',usuario);
