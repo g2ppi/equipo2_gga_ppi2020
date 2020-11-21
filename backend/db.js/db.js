e88f5cb8c46947e23e1e1724e30f27e724b80031
Samuelcoloradovelasquez <66636073+Samuelcoloradovelasquez@users.noreply.github.com>
2020-11-21 13:26:14 -0500

    Update db.js

--- backend/db.js/db.js
+++ backend/db.js/db.js
@@ -1,10 +1,10 @@
 const mysql =require('mysql');
 const mysqlConnection= mysql.createConnection({
     // db creada https://console.clever-cloud.com/
-host:'bprbeudfwg7eeipqbp9t-mysql.services.clever-cloud.com',
-user:'urappmxol0gahuzf',
-password:'piXeRzrY5CoTJ351xaU4',
-database:'bprbeudfwg7eeipqbp9t',
+host:'bpegmilzjbod5uj5dold-mysql.services.clever-cloud.com',
+user:'umsrzew6nbbwl8ff',
+password:'yD0lQug8dkxePI8dmoIG',
+database:'bpegmilzjbod5uj5dold',
 multipleStatements:true
 /*
 host:'181.133.136.83',
@@ -13,14 +13,14 @@ password:'123',
 database:'lab_mediatecnica',
 */
 });
+
 mysqlConnection.connect(function(err){
 if(err){
     console.error(err);
     return;
+
 }else{
-
-    console.log('base de datos conectada');
+console.log ('base de datos conectada ');
 }
-
 })
-module.exports = mysqlConnection;
+module.exports=mysqlConnection;
