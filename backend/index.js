b6749cf50e29053b95fe382254bc25f643f59387
Samuel Stiven Londoño Grisales <66587932+g2ppi@users.noreply.github.com>
2020-11-21 14:16:38 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,3 +12,4 @@ const rutina_usuario = require('./routes/rutina_usuario');// crud
 
 const usuario = require('./routes/usuario');// crud
 
+const tipousuario = require('./routes/tipousuario');// crud
